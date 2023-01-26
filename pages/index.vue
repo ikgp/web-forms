fb81e45797c513303c5227c6ec0a357285d754f7
AaronDewes <aaron.dewes@protonmail.com>
2023-01-26 19:13:57 +0000

    Add loading text

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
       <div ref="viewerContainer" class="container">
         <div ref="viewerElement">Wird geladen...</div>
       </div>
-      <button @click="onSubmit" class="save">Absenden</button>
+      <button @click="onSubmit" class="save">{{loading ? "Bitte warten..." : "Absenden"}}</button>
       <button @click="onDownload" class="download">Herunterladen</button>
     </div>
   </div>
@@ -22,12 +22,14 @@ import type { PDFViewer } from "pdfjs-dist/web/pdf_viewer";
 
 const viewerContainer = ref<HTMLDivElement | null>(null);
 const viewerElement = ref<HTMLDivElement | null>(null);
+const loading = ref(false);
 const submitted = ref(false);
 
 let viewer: PDFViewer | null = null;
 
 async function initPage() {
   submitted.value = false;
+  loading.value = false;
   const pdfJsLib = await import("pdfjs-dist");
   const pdfJs = await import("pdfjs-dist/web/pdf_viewer");
   pdfJsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.js";
@@ -52,6 +54,7 @@ async function initPage() {
 onMounted(initPage);
 
 async function onSubmit() {
+  loading.value = true;
   const pdf = await viewer!.pdfDocument?.saveDocument()!;
   const base64pdf = btoa(
     pdf.reduce((data, byte) => data + String.fromCharCode(byte), "")
@@ -65,7 +68,7 @@ async function onSubmit() {
     method: "POST",
     body: {
       data: base64pdf,
-      filename: `Anmeldung ${firstName}${lastName}.pdf`
+      filename: `Anmeldung ${firstName} ${lastName}.pdf`
     },
   });
   submitted.value = true;
@@ -78,7 +81,7 @@ async function onDownload() {
   );
   const link = document.createElement("a");
   link.href = `data:application/pdf;base64,${base64pdf}`;
-  link.download = "formular.pdf";
+  link.download = "Anmeldung IKGP.pdf";
   link.click();
 }
 </script>
