a3042da6aff8749096fd7d8d73296fdb518c7723
AaronDewes <aaron.dewes@protonmail.com>
2023-01-26 08:55:49 +0000

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    ncUser: process.env.NEXTCLOUD_USER,
+    ncPassword: process.env.NEXTCLOUD_PW,
+    public: {
+      ncUrl: process.env.NEXTCLOUD_SERVER,
+    },
+  },
+});
